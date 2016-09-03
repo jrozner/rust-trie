2e8d2c03857878061f291bd259dbde4f9a851b56
Josh Chase <jcjoshuachase@gmail.com>
2016-09-02 17:49:38 -0700

    Fix iterator (#1)

--- trie/src/main.rs
+++ trie/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     let trie: &mut Trie = &mut Trie::new();
     trie.insert("/usr/share");
     trie.insert("/etc");
+    trie.insert("/herp/derp");
+    trie.insert("/usr/lib");
+    trie.insert("/etc/openvpn");
+    println!("Trie: {:?}", trie);
 
     println!("{:?}", trie.prefix("/usr/share/dict/words"));
     println!("{:?}", trie.prefix("doc"));
@@ -23,19 +27,24 @@ fn main() {
     println!("{:?}", trie.contains("/etc"));
     println!("{:?}", trie.contains("/etc/"));
 
+    println!("things:");
     for item in trie.iter() {
         println!("{}", item);
     }
 }
 
+#[derive(Debug)]
 pub struct Trie {
     children: HashMap<char, Trie>,
-    boundary: bool
+    boundary: bool,
 }
 
 impl Trie {
     fn new() -> Trie {
-        Trie{children: HashMap::new(), boundary: false}
+        Trie {
+            children: HashMap::new(),
+            boundary: false,
+        }
     }
 
     fn insert<S: Into<String>>(&mut self, string: S) {
@@ -51,44 +60,82 @@ impl Trie {
     }
 
     fn iter(&self) -> Iter {
-        Iter{
-            stack: vec![StatePair{trie: self, keys: self.children.keys()}],
+        let mut keys: Vec<char> = self.children.keys().cloned().collect();
+        keys.sort_by(|a, b| b.cmp(a));
+        Iter {
+            stack: vec![StatePair {
+                            trie: self,
+                            keys: keys,
+                        }],
             current_word: Vec::new(),
         }
     }
 }
 
-pub struct Iter {
-    stack: Vec<StatePair>,
+pub struct Iter<'a> {
+    stack: Vec<StatePair<'a>>,
     current_word: Vec<char>,
 }
 
-struct StatePair {
-    trie: Trie,
-    keys: Keys,
+#[derive(Clone)]
+struct StatePair<'a> {
+    trie: &'a Trie,
+    keys: Vec<char>,
 }
 
-impl Iterator for Iter {
+impl<'a> Iterator for Iter<'a> {
     type Item = String;
 
     fn next(&mut self) -> Option<String> {
+        let mut direction = true;
         loop {
-            if let Some(current) = self.stack.last() {
-                if let Some(next_key) = current.keys.next() {
-                    self.current_word.push(next_key);
-                    let next = current.trie.children.get(next_key).unwrap();
-                    self.stack.push(StatePair{trie: next, keys: next.children.keys()});
-                    if current.boundary == true {
-                        return Some(self.current_word.iter().cloned().collect::<String>());
-                    }
-                    continue;
-                } else {
-                    self.stack.pop();
-                    self.current_word.pop();
-                }
-            } else {
+            if let None = self.stack.last() {
                 return None;
             }
+
+            let current = self.stack.last().unwrap().clone();
+            let boundary = current.trie.boundary;
+
+            let ret = if boundary == true {
+                Some(self.current_word.iter().cloned().collect::<String>())
+            } else {
+                None
+            };
+
+            let next_key = {
+                let current = self.stack.last_mut().unwrap();
+                current.keys.pop()
+            };
+
+            let next_direction = match next_key {
+                Some(key) => {
+                    self.current_word.push(key);
+                    let next = current.trie.children.get(&key).unwrap();
+
+                    let mut next_keys: Vec<char> = next.children.keys().cloned().collect();
+                    next_keys.sort_by(|a, b| b.cmp(a));
+
+                    self.stack.push(StatePair {
+                        trie: next,
+                        keys: next_keys,
+                    });
+                    true
+                }
+                None => {
+                    self.stack.pop();
+                    self.current_word.pop();
+                    false
+                }
+            };
+
+            if let Some(ret) = ret {
+                if direction {
+                    return Some(ret);
+                }
+            }
+
+            direction = next_direction;
+
         }
     }
 }
@@ -111,7 +158,7 @@ fn lookup(trie: &Trie, mut iter: Chars, prefix: bool) -> bool {
     if let Some(c) = iter.next() {
         match trie.children.contains_key(&c) {
             true => return lookup(trie.children.get(&c).unwrap(), iter, prefix),
-            false => return if prefix {trie.boundary} else {false}
+            false => return if prefix { trie.boundary } else { false },
         }
     }
 
